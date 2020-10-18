2de59b58e7028dcf4bc034a52791ad983333dca3
thesecretnix <59189977+thesecretnix@users.noreply.github.com>
2020-10-19 00:38:32 +0530

    Changes made

--- src/App.js
+++ src/App.js
@@ -1,26 +1,93 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
 
-function App() {
+import Card from 'react-bootstrap/Card';
+import CardDeck from 'react-bootstrap/CardDeck';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Columns from 'react-columns';
+import axios from 'axios';
+
+export default function App() {
+
+  const [data, setData] = useState([]);
+
+  const [country_data, setCountryData] = useState([]);
+
+  useEffect(() => {
+    axios.all
+      ([axios.get('https://disease.sh/v3/covid-19/all'),
+      axios.get('https://disease.sh/v3/covid-19/countries')
+      ])
+      .then(response_data => {
+        setData(response_data[0].data);
+        setCountryData(response_data[1].data);
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  }, []);
+
+  const date = new Date(parseInt(data.updated));
+  const data_value = date.toString();
+
+  const countries = country_data.map(
+    (data, item) => {
+      return (
+        <Card key={item} bg="light" text="dark" className="text-center" style={{ margin: '10px' }}>
+          <Card.Img variant="top" src={data.countryInfo.flag} />
+          <Card.Body>
+            <Card.Title>
+              {data.country}
+            </Card.Title>
+            <Card.Text>Total Cases {data.cases}</Card.Text>
+            <Card.Text>Active {data.active}</Card.Text>
+            <Card.Text>Recovered {data.recovered}</Card.Text>
+            <Card.Text>Deaths {data.deaths}</Card.Text>
+          </Card.Body>
+        </Card>
+      )
+    }
+  )
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ backgroundColor: 'grey'}}>
+      <CardDeck style={{backgroundColor: '#252930'}}>
+        <Card bg="danger" text="white" className="text-center">
+          <Card.Body>
+            <Card.Title>Active</Card.Title>
+            <Card.Text>
+              {data.active}
+            </Card.Text>
+          </Card.Body>
+          <Card.Footer>
+            <small>Last updated {data_value}</small>
+          </Card.Footer>
+        </Card>
+        <Card bg="secondary" text="white" className="text-center">
+          <Card.Body>
+            <Card.Title>Total Cases</Card.Title>
+            <Card.Text>
+              {data.cases}
+            </Card.Text>
+          </Card.Body>
+          <Card.Footer>
+            <small>Last updated {data_value}</small>
+          </Card.Footer>
+        </Card>
+        <Card bg="success" text="white" className="text-center">
+          <Card.Body>
+            <Card.Title>Recovered</Card.Title>
+            <Card.Text>
+              {data.recovered}
+            </Card.Text>
+          </Card.Body>
+          <Card.Footer>
+            <small>Last updated {data_value}</small>
+          </Card.Footer>
+        </Card>
+      </CardDeck>
+
+      <Columns>{countries}</Columns>
+
     </div>
   );
 }
-
-export default App;
